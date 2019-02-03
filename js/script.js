927f89eadfcf156adddb3f4c19f7bb2b1cfc139d
Faisal Ahmed <contact.faisalahmed@gmail.com>
2019-02-03 18:14:05 +0600

    SortBy Done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,58 @@
 $(() => {
-    // READ DATA
-    /*$.getJSON("json/data.json").done((data) => {
-
-    });*/
+    let aptData, sortBy, sortDir;
+    sortBy = 'date';
+    sortDir = 'desc';
     $('.hide').hide();
     $('nav>svg').on('click', () => {
         $('.hide').toggle(1000);
     });
     $.ajax({
         url: "js/data.json"
-    }).done(function (data) {
-        $datalisting(data);
+    }).done((data) => {
+        aptData = data;
+        $datalisting(aptData);
     });
     const $datalisting = data => {
-        //info = _.sortBy();
+        if (sortDir == 'asc') {
+            data = _.sortBy(data, sortBy);
+        } else {
+            data = _.sortBy(data, sortBy).reverse();
+        }
+
         $("#user-listing").loadTemplate('template.html', data, {
             complete: () => {
 
             }
         });
     }
+
+    $('.sort-menu .dropdown-item').on('click', function () {
+        let sortDropdown = $(this).attr('id');
+        switch (sortDropdown) {
+            case 'sort-name':
+                $('.sort-by').removeClass('active');
+                sortBy = 'name';
+                break;
+            case 'sort-email':
+                $('.sort-by').removeClass('active');
+                sortBy = 'email';
+                break;
+            case 'sort-date':
+                $('.sort-by').removeClass('active');
+                sortBy = 'date';
+                break;
+            case 'sort-asc':
+                $('.sort-dir').removeClass('active');
+                sortDir = 'asc';
+                break;
+            case 'sort-desc':
+                $('.sort-dir').removeClass('active');
+                sortDir = 'desc';
+                break;
+        }
+
+        $(this).addClass('active');
+        $datalisting(aptData);
+    });
 
 });
